07fad2cef6664d6ec8bb225a745686cdab0e8420
codeTheGlobe <imafidonosayanamwen@gmail.com>
2020-06-13 13:39:54 +0100

    working version login only

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -36,26 +36,26 @@ heroesRouter.route('/')
 .post(function(request,resp,next) {
 // const body = request.body;
 //body.func = ''
-// const sampleHeaders = {
-//   'user-agent': 'sampleTest',
-//   'Content-Type': 'text/xml;charset=UTF-8',
-//   'soapAction': 'https://online.firstcentralcreditbureau.com/FirstCentralNigeriaWebService/Login',
-// };
+const sampleHeaders = {
+  'user-agent': 'sampleTest',
+  'Content-Type': 'text/xml;charset=UTF-8',
+  'soapAction': 'https://online.firstcentralcreditbureau.com/FirstCentralNigeriaWebService/Login',
+};
 
 
 
-// //const xml = fs.readFileSync('routes/zipCodeEnvelope.xml', 'utf-8');
-// const xml = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:fir="https://online.firstcentralcreditbureau.com/FirstCentralNigeriaWebService">
-// <soapenv:Header/>
-// <soapenv:Body>
-//    <fir:Login>
-//       <!--Optional:-->
-//       <fir:UserName>${request.body.username}</fir:UserName>
-//       <!--Optional:-->
-//       <fir:Password>${request.body.password}</fir:Password>
-//    </fir:Login>
-// </soapenv:Body>
-// </soapenv:Envelope>`;
+//const xml = fs.readFileSync('routes/zipCodeEnvelope.xml', 'utf-8');
+const xml = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:fir="https://online.firstcentralcreditbureau.com/FirstCentralNigeriaWebService">
+<soapenv:Header/>
+<soapenv:Body>
+   <fir:Login>
+      <!--Optional:-->
+      <fir:UserName>${request.body.username}</fir:UserName>
+      <!--Optional:-->
+      <fir:Password>${request.body.password}</fir:Password>
+   </fir:Login>
+</soapenv:Body>
+</soapenv:Envelope>`;
 
 
 
@@ -79,68 +79,68 @@ heroesRouter.route('/')
 //console.log(xml);
 // var token;
 // usage of module
-// (async () => {
-//     const { response } = await soapRequest({ url: url, headers: sampleHeaders, xml: xml, timeout: 9000 }); // Optional timeout parameter(milliseconds)
-//     const { headers, body, statusCode } = response;
-//     // console.log(headers);
-//     // console.log(body);
-//     // console.log(statusCode);
+(async () => {
+    const { response } = await soapRequest({ url: url, headers: sampleHeaders, xml: xml, timeout: 9000 }); // Optional timeout parameter(milliseconds)
+    const { headers, body, statusCode } = response;
+    // console.log(headers);
+    // console.log(body);
+    // console.log(statusCode);
 
 
-//       var result = JSON.parse(xml2js.xml2json(body, {compact: true, spaces: 4}));
-//   var res = result['soap:Envelope']['soap:Body'].LoginResponse.LoginResult._text
-//   //console.log(res['soap:Envelope']['soap:Body'].LoginResponse.LoginResult._text);
-//   console.log(res);
-//     //resp.json(res);
-// token = res;
-  
-//   })();
-
-  const sampleHeaders2 = {
-    'user-agent': 'sampleTest',
-    'Content-Type': 'text/xml;charset=UTF-8',
-    'soapAction': 'https://online.firstcentralcreditbureau.com/FirstCentralNigeriaWebService/ConnectCommercialMatch',
-  };
-  
-  const xml2 = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:fir="https://online.firstcentralcreditbureau.com/FirstCentralNigeriaWebService">
-  <soapenv:Header/>
-  <soapenv:Body>
-     <fir:ConnectConsumerMatch>
-        <!--Optional:-->
-        <fir:DataTicket>EA00F09E9573FFB272D8036D6C1D09294DDD0CF62667822F9DB2AA5E4DF7651B5FB13CDB9C097B53286979F8A4DA44265D056CA04C83521F4E99193BC52A43CDEE6A058101B0E4EAE3FF90EA2D03D8C1A64B634043A937509301C7D2876AA96A7059B892411D224B9C2079A3210901F09A62840A7F5C9887013185BE433084D77CD912A4A9B10535BB39109966C5C2B5</fir:DataTicket>
-        <!--Optional:-->
-        <fir:EnquiryReason>Test</fir:EnquiryReason>
-        <!--Optional:-->
-        <fir:ConsumerName></fir:ConsumerName>
-        <!--Optional:-->
-        <fir:DateOfBirth></fir:DateOfBirth>
-        <!--Optional:-->
-        <fir:Identification>22377317600</fir:Identification>
-        <!--Optional:-->
-        <fir:AccountNumber></fir:AccountNumber>
-        <!--Optional:-->
-        <fir:ProductID></fir:ProductID>
-     </fir:ConnectConsumerMatch>
-  </soapenv:Body>
-</soapenv:Envelope>`;
-  //response.json(res)
-
-  (async () => {
-    const { response2 } = await soapRequest({ url: url, headers: sampleHeaders2, xml: xml2, timeout: 9000 }); // Optional timeout parameter(milliseconds)
-    const { headers, body, statusCode } = response2;
-    console.log(headers);
-    console.log(body);
-    console.log(statusCode);
-
-
-      var result2 = JSON.parse(xml2js.xml2json(body, {compact: true, spaces: 4}));
-  var res2 = result2['soap:Envelope']['soap:Body'].LoginResponse.LoginResult._text
+      var result = JSON.parse(xml2js.xml2json(body, {compact: true, spaces: 4}));
+  var res = result['soap:Envelope']['soap:Body'].LoginResponse.LoginResult._text
   //console.log(res['soap:Envelope']['soap:Body'].LoginResponse.LoginResult._text);
-  console.log(res2);
-    // resp.json(res);
-
+  console.log(res);
+    //resp.json(res);
+token = res;
   
   })();
+
+  // const sampleHeaders2 = {
+  //   'user-agent': 'sampleTest',
+  //   'Content-Type': 'text/xml;charset=UTF-8',
+  //   'soapAction': 'https://online.firstcentralcreditbureau.com/FirstCentralNigeriaWebService/ConnectConsumerMatch',
+  // };
+  
+//   const xml2 = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:fir="https://online.firstcentralcreditbureau.com/FirstCentralNigeriaWebService">
+//   <soapenv:Header/>
+//   <soapenv:Body>
+//      <fir:ConnectConsumerMatch>
+//         <!--Optional:-->
+//         <fir:DataTicket>EA00F09E9573FFB272D8036D6C1D09294DDD0CF62667822F9DB2AA5E4DF7651B5FB13CDB9C097B53286979F8A4DA44265D056CA04C83521F4E99193BC52A43CDEE6A058101B0E4EAE3FF90EA2D03D8C1A64B634043A937509301C7D2876AA96A7059B892411D224B9C2079A3210901F09A62840A7F5C9887013185BE433084D77CD912A4A9B10535BB39109966C5C2B5</fir:DataTicket>
+//         <!--Optional:-->
+//         <fir:EnquiryReason>Test</fir:EnquiryReason>
+//         <!--Optional:-->
+//         <fir:ConsumerName></fir:ConsumerName>
+//         <!--Optional:-->
+//         <fir:DateOfBirth></fir:DateOfBirth>
+//         <!--Optional:-->
+//         <fir:Identification>22377317600</fir:Identification>
+//         <!--Optional:-->
+//         <fir:AccountNumber></fir:AccountNumber>
+//         <!--Optional:-->
+//         <fir:ProductID></fir:ProductID>
+//      </fir:ConnectConsumerMatch>
+//   </soapenv:Body>
+// </soapenv:Envelope>`;
+  //response.json(res)
+
+  // (async () => {
+  //   const { response2 } = await soapRequest({ url: url, headers: sampleHeaders2, xml: xml2, timeout: 9000 }); // Optional timeout parameter(milliseconds)
+  //   const { headers, body, statusCode } = response2;
+  //   console.log(headers);
+  //   console.log(body);
+  //   console.log(statusCode);
+
+
+  //     var result2 = JSON.parse(xml2js.xml2json(body, {compact: true, spaces: 4}));
+  // var res2 = result2['soap:Envelope']['soap:Body'].LoginResponse.LoginResult._text
+  // //console.log(res['soap:Envelope']['soap:Body'].LoginResponse.LoginResult._text);
+  // console.log(res2);
+  //   // resp.json(res);
+
+  
+  // })();
 
 });
 
